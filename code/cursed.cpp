099296f71b1d656bc66f57a28579173274e751d5
rivten <rivten.grey@gmail.com>
2018-09-06 20:42:58 +0200

    add event handling and tile size

--- code/cursed.cpp
+++ code/cursed.cpp
@@ -7,20 +7,25 @@
 #endif
 
 #include "common.h"
+#include "math.h"
 
 #define SDL_CHECK(Call) {Assert(Call == 0);}
+
+global_variable bool GlobalRunning = true;
 
 int main(int ArgumentCount, char** Arguments)
 {
 	SDL_CHECK(SDL_Init(SDL_INIT_VIDEO));
 
-	u32 WindowWidth = 200;
-	u32 WindowHeight = 200;
+	u32 TileSize = 16;
+	v2i TileCount = V2i(80, 50);
+
+	v2i WindowSize = TileSize * TileCount;
 	u32 WindowFlags = SDL_WINDOW_SHOWN;
 
 	SDL_Window* Window =
 		SDL_CreateWindow("cursed", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
-			WindowWidth, WindowHeight, WindowFlags);
+			WindowSize.x, WindowSize.y, WindowFlags);
 	Assert(Window);
 
 	u32 RendererFlags = SDL_RENDERER_ACCELERATED;
@@ -28,10 +33,28 @@ int main(int ArgumentCount, char** Arguments)
 		SDL_CreateRenderer(Window, -1, RendererFlags);
 	Assert(Renderer);
 
-	SDL_SetRenderDrawColor(Renderer, 112, 128, 144, 255);
-	SDL_RenderClear(Renderer);
-	SDL_RenderPresent(Renderer);
-	SDL_Delay(3000);
+	while(GlobalRunning)
+	{
+		SDL_Event Event = {};
+		while(SDL_PollEvent(&Event))
+		{
+			switch(Event.type)
+			{
+				case SDL_QUIT:
+					{
+						GlobalRunning = false;
+					} break;
+				default:
+					{
+					} break;
+
+			}
+		}
+
+		SDL_SetRenderDrawColor(Renderer, 112, 128, 144, 255);
+		SDL_RenderClear(Renderer);
+		SDL_RenderPresent(Renderer);
+	}
 
 	SDL_Quit();
 
