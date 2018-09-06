e05d0be91c94bad5ffd39704bab27d9d6c36ad7d
rivten <rivten.grey@gmail.com>
2018-09-06 20:32:57 +0200

    basic window renderer creation

--- code/cursed.cpp
+++ code/cursed.cpp
@@ -18,11 +18,22 @@ int main(int ArgumentCount, char** Arguments)
 	u32 WindowHeight = 200;
 	u32 WindowFlags = SDL_WINDOW_SHOWN;
 
-	SDL_Window* Window = SDL_CreateWindow("cursed",
-			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
+	SDL_Window* Window =
+		SDL_CreateWindow("cursed", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
 			WindowWidth, WindowHeight, WindowFlags);
 	Assert(Window);
 
+	u32 RendererFlags = SDL_RENDERER_ACCELERATED;
+	SDL_Renderer* Renderer =
+		SDL_CreateRenderer(Window, -1, RendererFlags);
+	Assert(Renderer);
+
+	SDL_SetRenderDrawColor(Renderer, 112, 128, 144, 255);
+	SDL_RenderClear(Renderer);
+	SDL_RenderPresent(Renderer);
+	SDL_Delay(3000);
+
 	SDL_Quit();
+
 	return(0);
 }
